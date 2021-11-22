8a982ff593f2dfcc102c0abc5eff8402251466f0
jabertuhin <jabertuhin@gmail.com>
2021-11-23 00:15:02 +0600

    Add gunicorn config.

---
+++ gunicorn_config.py
@@ -0,0 +1,15 @@
+# source: https://github.com/benoitc/gunicorn/blob/master/examples/example_config.py
+import os
+
+port = os.getenv("PORT", 8080)
+bind = f"0.0.0.0:{port}"
+backlog = os.getenv("BACKLOG_NUM", 2048)
+
+
+workers = os.getenv("WORKER_NUM", 2)
+worker_class = 'uvicorn.workers.UvicornWorker'
+
+
+errorlog = '-'
+loglevel = 'INFO'
+accesslog = '-'
