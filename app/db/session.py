be51ce0c7a8918444e8b95dad5e9bf0a31f98043
jabertuhin <jabertuhin@gmail.com>
2022-01-15 00:08:14 +0600

    Add session.py

---
+++ app/db/session.py
@@ -0,0 +1,17 @@
+import os
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_SERVER = os.getenv("DB_SERVER")
+
+
+SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_SERVER}/{DB_NAME}"
+
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
