084d34c05c37f5c9271ba16992679568236d92fe
jabertuhin <jabertuhin@gmail.com>
2022-01-15 15:12:39 +0600

    Add routes and exceptions in app.

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 
-from app.routes import health
+from app.exceptions.handlers import add_exceptions_in_handler
+from app.routes import health, actor
 
-
-app = FastAPI()
+# TODO: Read api_version and title from config file.
+api_version = 0.1
+title = "DVD Rental"
+app = FastAPI(title=title, version=api_version)
 
 
 # including router in the app
 app.include_router(health.router)
+app.include_router(actor.router)
+
+
+# Add exceptions
+add_exceptions_in_handler(app)
